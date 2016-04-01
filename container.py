94379323e4291cad2279fe0d63163342ebb17381
espush <webmaster@espush.cn>
2016-04-01 14:24:48 +0800

    network .

--- container.py
+++ container.py
@@ -22,6 +22,9 @@ def container_main():
     os.environ['PATH'] = '/bin:/sbin:/usr/bin:/usr/sbin'
     os.chdir('/bin')
     mount_proc(private=False)
+    os.system('ip link set lo up')
+    os.system('ip link set veth1 up')
+    os.system('ip addr add 10.8.8.2/24 dev veth1')
     os.execv('/bin/sh', ['/bin/sh', ])
     print("something's wrong")
     return 123
@@ -39,7 +42,14 @@ def main():
     # 宿主配置
     mount_proc(private=True)
     os.system('read -p "Press any key to continue"')
-    pid = clone(container_main, SIGCHLD | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWUTS| CLONE_NEWNS)
+    pid = clone(container_main, SIGCHLD | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWUTS| CLONE_NEWNS | CLONE_NEWNET)
+    pread, pwrite = os.pipe()
+    # 网络配置
+    os.system("ip link add veth0 type veth peer name veth1")
+    os.system('ip link set veth1 netns %d' % pid)
+    os.system("ip link set veth0 up")
+    os.system("ip addr add 10.8.8.1/24 dev veth0")
+    os.close(pwrite)
     print('child process: [%d]' % pid)
     pidv, status = os.waitpid(pid, 0)
     print('pid: %d, status: %s' % (pidv, status))
